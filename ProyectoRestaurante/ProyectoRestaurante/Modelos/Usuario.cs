ffe092583bbf04fb407504e7b64137700669e7da
joell <116324165+JoelLandez@users.noreply.github.com>
2022-12-02 20:01:36 -0600

    Nuevo usuario
    una parte terminada del Nuevo Usuario

--- ProyectoRestaurante/ProyectoRestaurante/Modelos/Usuario.cs
+++ ProyectoRestaurante/ProyectoRestaurante/Modelos/Usuario.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,9 +9,13 @@ namespace Modelos
 {
     public class Usuario
     {
+        [Required(ErrorMessage = "El Codigo Usuario es Obligatorio")]
         public string CodigoUsuario { get; set; }
+        [Required(ErrorMessage = "El Nombre es Obligatorio")]
         public string Nombre { get; set; }
+        [Required(ErrorMessage = "El Rol es Obligatorio")]
         public string Rol { get; set; }
+        [Required(ErrorMessage = "La Clave es Obligatorio")]
         public string Clave { get; set; }
         public int EstaActivo { get; set; }
 
---
+++ ProyectoRestaurante/ProyectoRestaurante/ProyectoRestaurante/Pages/MisUsuarios/NuevoUsuario.razor.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Components;
+using Modelos;
+using ProyectoRestaurante.Interfaces;
+
+namespace ProyectoRestaurante.Pages.MisUsuarios
+{
+    public partial class NuevoUsuario
+    {
+        [Inject] private IUsuarioServicio usuarioServicio { get; set; }
+        [Inject] private NavigationManager navigationManager { get; set; }
+        private Usuario user = new Usuario();
+
+        protected async void Guardar()
+        {
+            if (string.IsNullOrEmpty(user.CodigoUsuario) || string.IsNullOrEmpty(user.Nombre) || string.IsNullOrEmpty(user.Clave)
+                || string.IsNullOrEmpty(user.Rol) || user.Rol == "Seleccionar")
+            {
+                return;
+            }
+
+            bool inserto = await usuarioServicio.Nuevo(user);
+
+            navigationManager.NavigateTo("/Usuario");
+        }
+
+        protected void Cancelar()
+        {
+            navigationManager.NavigateTo("/Usuario");
+        }
+
+    }
+}
+
+enum Roles
+{
+    Seleccionar,
+    Administrador,
+    Usuario
+}
