c28ed054859d17b4aaf61e5e2849376abf1ac6a8
joell <116324165+JoelLandez@users.noreply.github.com>
2022-12-02 18:45:23 -0600

    Menu Usuarios
    menu de usuario

---
+++ ProyectoRestaurante/ProyectoRestaurante/ProyectoRestaurante/Pages/MisUsuarios/Usuarios.razor.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Components;
+using Modelos;
+using ProyectoRestaurante.Interfaces;
+
+namespace ProyectoRestaurante.Pages.MisUsuarios
+{
+    public partial class Usuarios
+    {
+        [Inject] private IUsuarioServicio usuarioServicio { get; set; }
+
+        private IEnumerable<Usuario> lista { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            lista = await usuarioServicio.GetLista();
+        }
+    }
+}
